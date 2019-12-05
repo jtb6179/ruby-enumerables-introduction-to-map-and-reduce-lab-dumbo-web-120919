5b830fedec3e80acbf1f7d8fce13f10a9e864739
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 22:08:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,7 +55,7 @@ def reduce_to_all_true(source_array)
   
   counter = 0 
   while counter < source_array.size do
-       if source_array[counter] != true
+       if !source_array[counter] 
          return false
     counter += 1
   end

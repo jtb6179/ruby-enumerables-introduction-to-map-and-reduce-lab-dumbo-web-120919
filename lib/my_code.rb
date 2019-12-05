459407de2ccc42b35bb8ac3db46f0a79b68d32a2
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 21:13:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize(source_array)
   final_array = []
   counter = 0 
   while counter < source_array.size do
-    final_array.push
+    final_array.push(source_array[counter])
     counter += 1
   end
 end

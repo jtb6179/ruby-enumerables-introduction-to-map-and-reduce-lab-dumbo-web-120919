ed12e58da939730bce97eab4383ca5471d5902ad
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 21:29:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,13 @@ end
   
   
 def reduce_to_total(source_array, starting_point)
-  
+  total = []
+  counter = 0 
+  while counter < source_array.size do
+    final_array.push(source_array[counter] * -1)
+    counter += 1
+  end
+  return final_array
   
 end
 

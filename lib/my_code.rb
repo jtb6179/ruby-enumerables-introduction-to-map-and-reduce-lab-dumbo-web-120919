6b5ad151ebb8d3376aaa626ebd462dbf8f952abd
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 21:17:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,13 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  
+  final_array = []
+  counter = 0 
+  while counter < source_array.size do
+    final_array.push(source_array[counter] * 2)
+    counter += 1
+  end
+  return final_array
 end
   
   

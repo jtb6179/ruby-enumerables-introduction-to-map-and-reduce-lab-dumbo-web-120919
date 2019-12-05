70c0e19124bc2384d7acbe85b9388c058984c31b
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 21:34:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def map_to_no_change(source_array)
   final_array = []
   counter = 0 
   while counter < source_array.size do
-    final_array.push(source_array)
+    final_array.push(source_array[counter])
     counter += 1
   end
   return final_array
